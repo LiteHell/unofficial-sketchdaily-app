3307f0207f9929077a8e8168b40d39e37f789a19
LiteHell <litehell@litehell.info>
2022-12-01 00:34:18 +0900

    Add tests for Animal, News, Announcement api

---
+++ test/animal_api_test.dart
@@ -0,0 +1,67 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:sketchdaily/sketchdaily_api/sketchdaily_api.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  group('Animal', () {
+    test('Get animal without any options', () async {
+      Animal animal = (await Animal.getAnimal(AnimalOption()))!;
+      final image = await http.get(animal.uri);
+      expect(image.statusCode, 200);
+      expect(image.contentLength, greaterThan(0));
+      expect(image.headers['content-type'], startsWith('image/'));
+    });
+
+    test('Get animal with species option', () async {
+      for (final species in AnimalSpecies.values) {
+        printOnFailure('Testing animal with species option $species');
+        if (species == AnimalSpecies.bug) {
+          // There's no bug image on SketchDaily reference
+          continue;
+        }
+        Animal? animal = await Animal.getAnimal(AnimalOption(species: species));
+        expect(animal, isNotNull);
+        expect(animal!.classification.species, species);
+        final image = await http.get(animal.uri);
+        expect(image.statusCode, 200);
+        expect(image.contentLength, greaterThan(0));
+        expect(image.headers['content-type'], startsWith('image/'));
+      }
+    });
+
+    test('Get animal with category option', () async {
+      for (final category in AnimalCategory.values) {
+        printOnFailure('Testing animal with category option $category');
+        Animal? animal =
+            await Animal.getAnimal(AnimalOption(category: category));
+        expect(animal, isNotNull);
+        expect(animal!.classification.category, category);
+        final image = await http.get(animal.uri);
+        expect(image.statusCode, 200);
+        expect(image.contentLength, greaterThan(0));
+        expect(image.headers['content-type'], startsWith('image/'));
+      }
+    });
+
+    test('Get animal with viewAngle option', () async {
+      for (final viewAngle in ViewAngle.values) {
+        printOnFailure('Testing animal with viewAngle option $viewAngle');
+        Animal? animal =
+            await Animal.getAnimal(AnimalOption(viewAngle: viewAngle));
+        expect(animal, isNotNull);
+        expect(animal!.classification.viewAngle, viewAngle);
+        final image = await http.get(animal.uri);
+        expect(image.statusCode, 200);
+        expect(image.contentLength, greaterThan(0));
+        expect(image.headers['content-type'], startsWith('image/'));
+      }
+    });
+  });
+}
---
+++ test/news_api_test.dart
@@ -0,0 +1,50 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sketchdaily/sketchdaily_api/sketchdaily_api.dart';
+
+void main() {
+  group('News', () {
+    isNonEmptyNews(element) {
+      expect(element.content, isNotEmpty);
+      expect(element.id, isNotEmpty);
+      expect(element.title, isNotEmpty);
+    }
+
+    test('Get announcement', () async {
+      Announcement announcement = await Announcement.getAnnouncement();
+      expect(announcement.id, isNotEmpty);
+    });
+
+    test('Get news', () async {
+      List<News> news = await News.getNews();
+
+      news.forEach(isNonEmptyNews);
+    });
+
+    test('Get news with limit', () async {
+      for (var i = 1; i <= 3; i++) {
+        List<News> news = await News.getNews(limit: i);
+
+        expect(news.length, i);
+        news.forEach(isNonEmptyNews);
+      }
+    });
+
+    test('Get news with limit and offset', () async {
+      List<News> secondNews = await News.getNews(offset: 2, limit: 1);
+      List<News> thirdNews = await News.getNews(offset: 3, limit: 1);
+      List<News> news = await News.getNews(offset: 1, limit: 3);
+      expect(news.length, 3);
+      expect(news[1].id, secondNews.first.id);
+      expect(news[2].id, thirdNews.first.id);
+
+      news.forEach(isNonEmptyNews);
+    });
+  });
+}
